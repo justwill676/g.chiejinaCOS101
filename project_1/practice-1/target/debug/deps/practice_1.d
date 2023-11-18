e77942e7e0d7b9d7a4f950f854e50b72a7005794
justwill676 <148973533+justwill676@users.noreply.github.com>
2023-11-18 20:38:20 +0100

    Delete project_1/practice-1 directory

--- project_1/practice-1/target/debug/deps/practice_1.d
+++
@@ -1,5 +0,0 @@
-C:\Users\User\Documents\g.chiejinaCOS101\project_1\practice-1\target\debug\deps\practice_1.exe: src\main.rs
-
-C:\Users\User\Documents\g.chiejinaCOS101\project_1\practice-1\target\debug\deps\practice_1.d: src\main.rs
-
-src\main.rs:
--- project_1/practice-1/target/debug/practice-1.d
+++
@@ -1,1 +0,0 @@
-C:\Users\User\Documents\g.chiejinaCOS101\project_1\practice-1\target\debug\practice-1.exe: C:\Users\User\Documents\g.chiejinaCOS101\project_1\practice-1\src\main.rs
